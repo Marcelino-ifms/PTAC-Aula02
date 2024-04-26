1892134fbcbb4758112d3bdd59af4632d446d943
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2024-04-26 10:30:07 -0300

    DeletarIndex

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,89 +1,74 @@
+
 import { useState } from "react";
 
 export default function Main(){
-  const[nome, setNome] = useState("") //(0) number  // (true) boolean
-  const[telefone, setTelefone] = useState(0)
-  const[email, setEmail] = useState("")
-  const[listaContatos, setListaContatos] = useState([]) //lista usamos cochetes[]
-
+  const[nome, setNome] = useState("");
+  const[telefone, setTelefone] = useState(0);
+  const[email, setEmail] = useState("");
+  const[listaContatos, setListaContatos] = useState([]);
 
   const registrar = (event) => {
     event.preventDefault();
     setListaContatos([...listaContatos,
-      
       {
         nomeSalvo: nome,
         telefoneSalvo: telefone,
         emailSalvo: email,
       }
-
     ]);
   };
 
   const deletar = (index) => {
-    
-  }
+    const novosContatos = [...listaContatos];
+    novosContatos.splice(index, 1);
+    setListaContatos(novosContatos);
+  };
 
-  console.table(listaContatos)
-    return(
-      <main>
+  console.table(listaContatos);
 
-                <form action=""  onSubmit={registrar}>
-                <label htmlFor="nome"> Nome:  </label>
-                <input
-                type="text"
-                name="" id=""
-                value={nome}
-                onChange={(event)=> setNome(event.target.value)}
-                />
-
-                <button>Cadastre-se</button>
-          
-          
-
-         <br/>
-         <br/>
-    
-
-         <label htmlFor="telefone"> Telefone:  </label>
-
-<label htmlFor="telefone"> Telefone:  </label>
-
-         <input
-         type="tel"
-         name="" id=""
-         value={telefone}
-         onChange={(event)=> setTelefone(event.target.value)}
-         />
-
-          <button>Cadastre-se</button>
+  return(
+    <div className="Registro">
+    <main>
+      <form action=""  onSubmit={registrar}>
+        <label htmlFor="nome"> Nome:  </label>
+        <input
+          type="text"
+          name="" id=""
+          value={nome}
+          onChange={(event)=> setNome(event.target.value)}
+        />
+       
+        <label htmlFor="telefone"> Telefone:  </label>
+        <input
+          type="tel"
+          name="" id=""
+          value={telefone}
+          onChange={(event)=> setTelefone(event.target.value)}
+        />
+        
+        <label htmlFor="email"> Email:  </label>
+        <input
+          type="email"
+          name="" id=""
+          value={email}
+          onChange={(event)=> setEmail(event.target.value)}
+        />
+     
+        <button>Cadastre-se</button>
+      </form>
+    <div className="DadosSalvos">
+      {listaContatos.map((contato, index) => 
+        <div key={index}>
+          <p>{contato.nomeSalvo}</p>
+          <p>{contato.telefoneSalvo}</p>
+          <p>{contato.emailSalvo}</p>
+        <div className="Botao">  <button onClick={() => deletar(index)}>Deletar</button>  </div>
+        </div>
       
-         
-           <br/>
-           <br/>
-
-
-                <label htmlFor="email"> Email:  </label>
-                <input
-                type="email"
-                name="" id=""
-                value={email}
-                onChange={(event)=> setEmail(event.target.value)}
-                />
-                
-                  <button>Cadastre-se</button>
-         
-
-         </form>
-
-         {listaContatos.map((contato, index)=> 
-         <div key={index}>
-               <p>{contato.nomeSalvo}</p>
-               <p>{contato.telefoneSalvo}</p>
-               <p>{contato.emailSalvo}</p>
-               <button onClick={deletar}>Deletar</button>
-         </div>
-         )}
-      </main>
-    );
+      )}
+      </div>
+    </main>
+    </div>
+  );
 }
+
